fc3684c20d66e83280e4fb9465d8aa3212d0c453
sharmasourab93 <sharmasourab93@gmail.com>
2020-07-05 19:48:08 +0530

    Nginx & Docker Integrated

---
+++ flask_kb/entrypoint.sh
@@ -0,0 +1,3 @@
+exec gunicorn --bind 0.0.0.0:5000 run -w 1
+
+
--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-#echo "Killing Old Docker Containers"
-#docker-compose rm -fs 
+echo "Killing Old Docker Containers"
+docker-compose rm -fs 
 
 echo "Building Docker Containers"
-# docker-compose up --build -d
-docker build -t flaskkb_nginx:1.3 .
+docker-compose up --build -d
+#docker build -t flaskkb_nginx:1.3 .
